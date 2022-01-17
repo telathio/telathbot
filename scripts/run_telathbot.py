3dcfc1abc484a9ff8330f2038cdba705eadc1f49
ivanklee86 <ivanklee86@gmail.com>
2022-01-17 13:58:29 -0500

    Add logging.

--- scripts/run_telathbot.py
+++ scripts/run_telathbot.py
@@ -7,17 +7,19 @@ def main(
         ..., help="Fully formed URL (i.e. http://telathbot.com", envvar="TELATHBOT_URL"
     )
 ):
+    typer.echo("Starting TelathBot SafetyTools checks.")
     ip_check_resp = httpx.post(f"{url}/metadata/check/ip", data={})
     if ip_check_resp.status_code != 200:
         raise Exception("Error checking public IP.")
 
+    typer.echo("TelathBot public IP unchanged, scraping SafetyTools uses.")
     safetytools_params = {"notify": True, "persist": True}
     safetools_response = httpx.get(
         f"{url}/safetytools/level/red", params=safetytools_params
     )
     if safetools_response.status_code != 200:
         raise Exception("Error reporting on safetytools.")
-
+    typer.echo("Everything complete, exiting!.")
 
 if __name__ == "__main__":
     typer.run(main)
