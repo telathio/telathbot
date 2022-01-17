21fb55a3d3c643f7bc066ac11065e087c8deea81
Ivan Lee <ivanklee86@gmail.com>
2022-01-17 14:40:02 -0500

    Hard code port.

--- telathbot/main.py
+++ telathbot/main.py
@@ -4,5 +4,4 @@ from telathbot import app
 from telathbot.config import get_settings
 
 if __name__ == "__main__":
-    config = get_settings()
-    uvicorn.run(app, host="0.0.0.0", port=config.telathbot_port)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
