77a24257676dc613f8cf5dcb09df0d11a1cbc12a
Ivan Lee <ivanklee86@gmail.com>
2022-01-17 15:48:43 -0500

    HOTFIX: script fixes.

--- scripts/run_telathbot.py
+++ scripts/run_telathbot.py
@@ -8,14 +8,18 @@ def main(
     )
 ):
     typer.echo("Starting TelathBot SafetyTools checks.")
-    ip_check_resp = httpx.post(f"{url}/metadata/check/ip", data={})
+    ip_check_resp = httpx.post(
+        f"{url}/metadata/check/ip",
+        headers={"content-type": "application/json"},
+        json={"ip": ""},
+    )
     if ip_check_resp.status_code != 200:
         raise Exception("Error checking public IP.")
 
     typer.echo("TelathBot public IP unchanged, scraping SafetyTools uses.")
     safetytools_params = {"notify": True, "persist": True}
     safetools_response = httpx.get(
-        f"{url}/safetytools/level/red", params=safetytools_params
+        f"{url}/safetytools/uses/red", params=safetytools_params
     )
     if safetools_response.status_code != 200:
         raise Exception("Error reporting on safetytools.")
