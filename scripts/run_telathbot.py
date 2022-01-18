efc4f2380c1fda0bb59ea8de250dad75cf0c5b1e
Ivan Lee <ivanklee86@gmail.com>
2022-01-18 03:29:44 -0500

    Reformat files.

--- scripts/run_telathbot.py
+++ scripts/run_telathbot.py
@@ -17,19 +17,15 @@ def main(
         raise Exception("Error checking public IP.")
 
     typer.echo("TelathBot public IP unchanged, scraping SafetyTools uses.")
-    safetools_scrape_response = httpx.post(
-        f"{url}/safetytools/uses/red"
-    )
+    safetools_scrape_response = httpx.post(f"{url}/safetytools/uses/red")
     if safetools_scrape_response.status_code != 200:
         raise Exception("Error scraping on safetytools.")
 
     typer.echo("TelathBot scraping complete, notifying of uses.")
-    safetools_scrape_response = httpx.post(
-        f"{url}/safetytools/notify/red"
-    )
+    safetools_scrape_response = httpx.post(f"{url}/safetytools/notify/red")
     if safetools_scrape_response.status_code != 200:
         raise Exception("Error notifying on safetytools.")
-    
+
     typer.echo("Everything complete, exiting!.")
 
 
--- telathbot/main.py
+++ telathbot/main.py
@@ -4,7 +4,11 @@ from uvicorn.config import LOGGING_CONFIG
 from telathbot import app
 
 if __name__ == "__main__":
-    LOGGING_CONFIG["formatters"]["default"]["fmt"] = "%(asctime)s [%(name)s] %(levelprefix)s %(message)s"
-    LOGGING_CONFIG["formatters"]["access"]["fmt"] = '%(asctime)s [%(name)s] %(levelprefix)s %(client_addr)s - "%(request_line)s" %(status_code)s'
+    LOGGING_CONFIG["formatters"]["default"][
+        "fmt"
+    ] = "%(asctime)s [%(name)s] %(levelprefix)s %(message)s"
+    LOGGING_CONFIG["formatters"]["access"][
+        "fmt"
+    ] = '%(asctime)s [%(name)s] %(levelprefix)s %(client_addr)s - "%(request_line)s" %(status_code)s'
 
     uvicorn.run(app, host="0.0.0.0", port=8000)
